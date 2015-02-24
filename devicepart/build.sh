f2847d4c385251e035645971bac0fab7059cd37a
Theo Julienne <theo.julienne@gmail.com>
2015-02-24 12:19:32 -0800

    Add devicepart modification.

---
+++ devicepart/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [[ ! -f device_part_ninjasphere.tar.xz ]]; then
+	wget https://firmware.sphere.ninja/snappy/device_part_ninjasphere.tar.xz
+fi
+
+rm -rf tmp
+mkdir -p tmp
+cd tmp
+tar xJvf ../device_part_ninjasphere.tar.xz
+
+cd boot
+rm uEnv*.txt
+cp ../../src/uEnv.txt .
+cd ..
+
+tar cJvf ../device_part_ninjasphere_mod.tar.xz *
+cd ..
