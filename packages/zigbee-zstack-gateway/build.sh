308ddd9ea331a75d877883f442250bedf379bad6
Jon Seymour <jon.seymour@gmail.com>
2015-03-20 10:09:35 +1100

    Add support for zigbee-zstack-gateway.

---
+++ packages/zigbee-zstack-gateway/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+APT_URL=https://s3.amazonaws.com/ninjablocks-apt-repo
+APT_DEB=pool/main/z/zigbee-zstack-gateway/zigbee-zstack-gateway_0.1.0~trustyspheramid-8_armhf.deb
+APT_FILE=$(basename "$APT_DEB")
+APT_SHA1=1116f4d0beeef172b22843e79a90c43ab70f398b
+
+. ../packaging.sh
+
+fetch-apt "${APT_URL}" "${APT_DEB}" "${APT_FILE}" "${APT_SHA1}"
+
+begin-build-staging
+
+dpkg -x "${APT_FILE}" ${STAGING_DIR}
+
+rm -rf ${STAGING_DIR}/etc/
+rsync -rav ${STAGING_DIR}/opt/zigbee-zstack-gateway/ ${STAGING_DIR}/
+rsync -rav ${STAGING_DIR}/usr/lib/ ${STAGING_DIR}/servers/
+sed -i.bak "/DATABASE_PATH=/d" ${STAGING_DIR}/servers/nwkmgr_config.ini
+rm ${STAGING_DIR}/servers/nwkmgr_config.ini.bak
+echo "DATABASE_PATH=./dbpath" >> ${STAGING_DIR}/servers/nwkmgr_config.ini
+
+# location fixups
+
+apply-template-staging
+apply-version $(version-from-deb "$APT_FILE")
+
+snappy-build-staging
+
+#clean-build-staging
